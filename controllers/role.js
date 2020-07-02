c13d96cc3fef01a04b949340f0521ce9043d6930
wine1 <zqjm123456@qq.com>
2020-07-03 01:04:48 +0800

    联通数据库

--- controllers/role.js
+++ controllers/role.js
@@ -1,33 +1,43 @@
 
 const {mysql} = require('../qcloud.js')
 
-async function getRoleDetail(ctx) {
-    try {
-        const {id}=ctx.query;
-        const data = await mysql('users').
-        select('*').
-        where({ id });
-        ctx.state.code = 0;
-        ctx.state.data = data[0];
-        console.log(ctx.state.data)
-    }
-    catch(error){
-        console.log('error',error)
-    }
-}
+// async function getRoleDetail(ctx) {
+//     try {
+//         const {id}=ctx.query;
+//         const data = await mysql('users').
+//         select('*').
+//         where({ id });
+//         ctx.state.code = 0;
+//         ctx.state.data = data[0];
+//         console.log(ctx.state.data)
+//     }
+//     catch(error){
+//         console.log('error',error)
+//     }
+// }
 
-// post方法
-async function add(ctx) {
-    try {
-        const { name, fullName } = ctx.request.body;
-        const data = await mysql('users').insert({ username:'天' });
-        console.log(data)
-        ctx.state.code = 0;
-        ctx.state.data = { id: data[0] };
-    } catch (e) {
-        ctx.state.code = -1;
-        throw new Error(e);
-    }
-}
+// // post方法
+// async function add(ctx) {
+//     try {
+//         const { name, fullName } = ctx.request.body;
+//         const data = await mysql('users').insert({ username:'天' });
+//         console.log(data)
+//         ctx.state.code = 0;
+//         ctx.state.data = { id: data[0] };
+//     } catch (e) {
+//         ctx.state.code = -1;
+//         throw new Error(e);
+//     }
+// }
 
-add()
+// add()
+
+
+mysql('users').insert({username: 'b',password:'1234'})
+.then(res=>{
+    console.log('插入数据库成功')
+}).catch(ERROR=>{
+    console.log(error)
+});
+
+// console.log(mysql('users').select('*'))
