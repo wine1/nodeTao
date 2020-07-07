964b8a1f5c3d6a2f80d3eb3a0abfedf69cd584c3
tianjingjing1 <jingjing1@eastmoney.com>
2020-07-07 17:21:36 +0800

    基本完成

--- controllers/goods.js
+++ controllers/goods.js
@@ -2,7 +2,6 @@ const { mysql } = require("../qcloud.js");
 
 async function getGoodsList(ctx) {
   try {
-      console.log('ctx',ctx.then(res => console.log('res', res)))
     // const { id } = ctx.query;
     const data = await mysql("goods").select('*')
     console.log('111',ctx.state);
@@ -13,9 +12,20 @@ async function getGoodsList(ctx) {
     throw new Error(e);
   }
 }
-
-// getGoodsList()
+async function getGood(ctx) {
+    try {
+      const { id } = ctx.query;
+      const data = await mysql("goods").select('*').where({id})
+      console.log('111',ctx.state);
+      ctx.state.code = 0;
+      ctx.state.data = data;
+    } catch (e) {
+      ctx.state.code = -1;
+      throw new Error(e);
+    }
+  }
 
 module.exports = {
-  getGoodsList
+  getGoodsList,
+  getGood
 };
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const router = require('koa-router')();
 const controllers = require('../controllers')
 
 router.post('/login', controllers.login.signIn) //登录
-router.get('/list', controllers.goods.getGoodsList) //获取物品列表
+router.get('/goodsList', controllers.goods.getGoodsList) //获取物品列表
+router.get('/good', controllers.goods.getGood) //根据id获取商品
 
 module.exports = router
